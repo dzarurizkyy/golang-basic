13cd5a0b290dfff03283a009adc4c20de68a1426
Dzaru Rizky Fathan Fortuna <dzarurizkybusiness@gmail.com>
2025-01-12 20:55:43 +0700

    section 23 : empty interface

---
+++ 024-empty-interface/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+/*
+	Interface Kosong (interface{})
+	- Interface yang tidak memiliki deklarasi method satupun, hal ini membuat secara otomatis semua tipe data akan menjadi implementasinya
+	- Memiliki type alias bernama any
+*/
+
+func Ups() any {
+	return "OK"
+}
+
+func main() {
+	fmt.Println(Ups())
+}
