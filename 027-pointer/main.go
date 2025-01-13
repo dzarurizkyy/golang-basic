6c0481ede08eb8e4cfe82cc4c13400a36958ae78
Dzaru Rizky Fathan Fortuna <dzarurizkybusiness@gmail.com>
2025-01-13 15:22:07 +0700

    section 27 : pointer

---
+++ 027-pointer/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	city, province, country string
+}
+
+func main() {
+	address1 := Address{"Kediri", "Jawa Timur", "Indonesia"}
+
+	/* Pointer 
+	   Kemampuan membuat reference ke lokasi data di memory yang sama, tanpa menduplikasi data yang sudah ada (pass by reference)
+	*/
+	address2 := &address1
+
+	address2.city = "Jombang"
+
+	fmt.Println(address1)
+	fmt.Println(address2)
+}
