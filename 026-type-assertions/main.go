9e034b57c4e87d4181e7fc0ec9ac3bde7b165ff4
Dzaru Rizky Fathan Fortuna <dzarurizkybusiness@gmail.com>
2025-01-13 15:12:43 +0700

    section 26 : type assertions

---
+++ 026-type-assertions/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func random() interface{} {
+	return true
+}
+
+func main() {
+	result := random()
+
+	/*
+		Type Assertions
+		- Kemampuan mengubah tipe data menjadi tipe yang diinginkan
+		- Sering digunakan ketika bertemu interface kosong
+	*/
+	
+	switch result.(type) {
+	case string:
+		fmt.Println("String:", result)
+	case int:
+		fmt.Println("Int:", result)
+	default:
+		fmt.Println("Unknown:", result)
+	}
+}
